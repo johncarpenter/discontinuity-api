28c4beef44871a8cbbfcdc1200b7cb43914be3f6
John Carpenter <john.carpenter@blacksquare.io>
2023-09-15 18:17:45 -0600

    adding website

---
+++ website/pages/_app.js
@@ -0,0 +1,25 @@
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import 'katex/dist/katex.css'
+
+import { ThemeProvider } from 'next-themes'
+
+import Head from 'next/head'
+
+import siteMetadata from '@/data/siteMetadata'
+import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
+}
