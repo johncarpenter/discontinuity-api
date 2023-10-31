bff962a450b1d06b69cfe6886bb5507b7116e64c
John Carpenter <john.carpenter@blacksquare.io>
2023-10-31 18:25:16 -0500

    cleaning up the flowdb

--- terraform/database.tf
+++ terraform/database.tf
@@ -22,11 +22,16 @@ resource "google_sql_database" "default" {
   charset  = "utf8"
 }
 
-
 resource "google_sql_user" "users" {
   name     = "postgres"
   instance = google_sql_database_instance.default.name
   password = var.postgres_password
+}
+
+resource "google_sql_database" "flow" {
+  name     = "flowise"
+  instance = google_sql_database_instance.default.name
+  charset  = "utf8"
 }
 
 resource "google_sql_user" "vectordb_user" {
